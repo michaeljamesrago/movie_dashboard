7e37237283fe6ced7e58865f9b0d014b9aff0fd3
Michael Rago <michael.rago@gmail.com>
2022-09-05 16:45:38 -0500

    Added simple caching capability for API responses.

--- src/lib/apiClient.js
+++ src/lib/apiClient.js
@@ -1,29 +1,44 @@
-import { mockAPI } from '../data'
 import axios from "axios"
 const apiClient = {
     fetchTopMovies(callback) {
-        return new Promise((resolve, reject) => {
-            const data = mockAPI.getTop()
-            setTimeout(() => {
-                resolve(data);
-            }, 2000)
-        }).then(response => {
-            callback(response)
-        })
-    }, 
-    searchMovies(searchterm, callback) {
-        // return new Promise((resolve, reject) => {
-        //     const data = mockAPI.getSearch()
-        //     setTimeout(() => {
-        //         resolve(data);
-        //     }, 2000)
-        // }).then(response => {
-        //     callback(response)
-        // })
-        axios.get(`https://imdb-api.com/en/API/SearchMovie/k_q246kim4/${searchterm}`)
-        .then(response => {
+        const url = 'https://imdb-api.com/en/API/MostPopularMovies/k_q246kim4'
+        let cachedResponse = localStorage.getItem('topMovies') || '{}'
+        cachedResponse = JSON.parse(cachedResponse)
+        if (!!cachedResponse.cachedTime && Date.now() - cachedResponse.cachedTime < 604800000) {
+            console.log("Cached Response for top Movies")
+            callback(cachedResponse.data)
+        } else {
+          axios.get(url)
+          .then(response => {
+            const now = Date.now()
+            const cacheItem = {
+                cachedTime: now,
+                data: response.data
+            }
+            localStorage.setItem('topMovies', JSON.stringify(cacheItem))
             callback(response.data)
-        })
+          })
+        }
+    },     
+    searchMovies(searchterm, callback) {
+        const url = `https://imdb-api.com/en/API/SearchMovie/k_q246kim4/${searchterm}`
+        let cachedResponse = localStorage.getItem(`searchMovies_${url}`) || '{}'
+        cachedResponse = JSON.parse(cachedResponse)
+        if (!!cachedResponse.cachedTime && Date.now() - cachedResponse.cachedTime < 604800000) {
+            console.log("Cached Response for Search Movies")
+            callback(cachedResponse.data)
+        } else {
+          axios.get(url)
+          .then(response => {
+            const now = Date.now()
+            const cacheItem = {
+                cachedTime: now,
+                data: response.data
+            }
+            localStorage.setItem(`searchMovies_${url}`, JSON.stringify(cacheItem))
+            callback(response.data)
+          })
+        }
     }
 }
 
